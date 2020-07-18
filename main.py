c3759305e90f3e8b2e28388ea5c79ea03ab486aa
viktaur <victorgabal@gmail.com>
2020-07-18 12:51:22 +0200

    U

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import hashlib
 
+loop = True
+
+def skBinary():
+    inputBin = input("Introduce the binary secret key: ")
+
+def skHex():
+    inputHex = input("Introduce the hexadecimal secret key: ")
+
+def skB58():
+    inputB58 = input("Introduce the B58Check secret key: ")
+
+def inputloop():
+    sk_type = input("Please select the secret key format. [B] for binary, [H] for hexadecimal or [C] for B58Check: ")
+    while True:
+        if sk_type in ["B", "b"]:
+            skBinary()
+        elif sk_type in ["H", "h"]:
+            skHex()
+        elif sk_type in ["C", "c"]:
+            skB58()
+        else:
+            print("Please try again")
+            inputloop()
+
 def start():
     print("Welcome to btcaddress generator! Please consider reading the README.md file before using it for the first time.")
     input("Press Enter to continue...")
-    sk_type = input("Please select the private key format. [B] for binary, [H] for hexadecimal or [C] for B58Check")
-    if sk_type in ["B", "b"]:
-        skBinary()
-    elif sk_type in ["H", "h"]:
-        skHex()
-    elif sk_type in ["C", "c"]:
-        skB58()
-    else:
-        print("Please try again")
+    inputloop()
+
+
+start()
