cd7a1817edd528fb01d0e3d049139b52dc4f87d5
viktaur <victorgabal@gmail.com>
2020-07-26 20:26:18 +0200

    u

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import hashlib
 
-# def hexa(a, b):
-    # return hex(a, b)
-
-print(hex(int("000101", 2)))
-
-def skBinary():
-    bin = input("Introduce the binary secret key: ")
-    hex = format(int(bin, 2), 'x').upper()
-    print(hex)
+def skB58():
 
 def skHex():
-    inputHex = input("Introduce the hexadecimal secret key: ")
+    format(hex)
 
-def skB58():
-    inputB58 = input("Introduce the B58Check secret key: ")
+def skBinary():
+    print("Binary secret key:", bin)
+    print(hex)
 
 def inputloop():
     sk_type = input("Please select the secret key format. [B] for binary, [H] for hexadecimal or [C] for B58Check: ")
     while True:
         if sk_type in ["B", "b"]:
+            bin = input("Introduce the binary secret key: ")
+            hex = format(int(bin, 2), 'x').upper()
             skBinary()
             break
         elif sk_type in ["H", "h"]:
+            hex = input("Introduce the hexadecimal secret key: ")
             skHex()
             break
         elif sk_type in ["C", "c"]:
+            B58 = input("Introduce the B58Check secret key: ")
             skB58()
             break
         else:
