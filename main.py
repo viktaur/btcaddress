7b508c2100672e905efb0888fd50289644f77406
viktaur <victorgabal@gmail.com>
2020-07-26 22:25:00 +0200

    Update (unstable)

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 import hashlib
-
-def skB58():
-
-def skHex():
-    format(hex)
+import math
 
 def skBinary():
-    print("Binary secret key:", bin)
-    print(hex)
+    bin = input("Introduce the binary secret key: ")
+    hex = format(int(bin, 2), 'x').upper()
+
+    print("DO NOT SHARE WITH ANYONE")
+    print("The binary secret key is:", bin)
+    print("The hexadecimal secret key is:", hex)
+
+def skHex():
+    hex = input("Introduce the hexadecimal secret key: ")
+
+def skB58():
+    B58 = input("Introduce the B58Check secret key: ")
+
+def skRandom():
 
 def inputloop():
-    sk_type = input("Please select the secret key format. [B] for binary, [H] for hexadecimal or [C] for B58Check: ")
+    sk_type = input("Please select the input secret key format. [B] for binary, [H] for hexadecimal, [C] for B58Check or [R] if you want to generate a random key")
     while True:
         if sk_type in ["B", "b"]:
-            bin = input("Introduce the binary secret key: ")
-            hex = format(int(bin, 2), 'x').upper()
             skBinary()
             break
         elif sk_type in ["H", "h"]:
-            hex = input("Introduce the hexadecimal secret key: ")
             skHex()
             break
         elif sk_type in ["C", "c"]:
-            B58 = input("Introduce the B58Check secret key: ")
             skB58()
             break
+        elif sk_type in ["R", "r"]:
+            skRandom()
         else:
             print("Please try again")
             inputloop()
