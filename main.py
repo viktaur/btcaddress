f667e496e3c86f334ae91e16585d8f7caa589e46
viktaur <victorgabal@gmail.com>
2020-07-28 12:18:50 +0200

    unstable

--- main.py
+++ main.py
@@ -10,15 +10,35 @@ import bitcoin
 def clear():
     subprocess.Popen("cls" if platform.system() == "Windows" else "clear", shell=True)
 
+def base58(address_hex):
+    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+    b58_string = ""
+    # Get the number of leading zeros
+    leading_zeros = len(address_hex) - len(address_hex.lstrip("0"))
+    # Convert hex to decimal
+    address_int = int(address_hex, 16)
+    # Append digits to the start of string
+    while address_int > 0:
+        digit = address_int % 58
+        digit_char = alphabet[digit]
+        b58_string = digit_char + b58_string
+        address_int //= 58
+    # Add ‘1’ for each 2 leading zeros
+    ones = leading_zeros // 2
+    for one in range(ones):
+        b58_string = "1" + b58_string
+    return b58_string
+
 def skBinary():
     bin = input("Introduce the binary secret key: ")
     hex = format(int(bin, 2), 'x').upper()
     b58 = bitcoin.hex_to_b58check(hex, magicbyte=128)
-    # wif =
-    print(BRed + "DO NOT SHARE WITH ANYONE")
+    wif = base58(b58)
+    print(BRed + "PRIVATE INFORMATION (DO NOT SHARE WITH ANYONE):")
     print(BRed + "The binary secret key is:" + Red, bin)
     print(BRed + "The hexadecimal secret key is:" + Red, hex)
-    print(BRed + "The B58Check secret key is:" + Red, b58)
+    print(BRed + "The B58Check WIF compressed secret key is:" + Red, b58)
+    print(BRed + "WIF (public key) is:" + Red, wif)
     quit()
 
 def skHex():
