071cfb3d84a5bc321881df5c99b317551bdbf9d6
viktaur <victorgabal@gmail.com>
2020-07-30 17:57:54 +0200

    semistable

--- main.py
+++ main.py
@@ -6,52 +6,46 @@ import platform
 import random
 import time
 import bitcoin
-import binascii
+import argparse
+
+from binascii import hexlify, unhexlify
+from struct import Struct
+from utils import g, b58encode, b58decode
+
 
 def clear():
     subprocess.Popen("cls" if platform.system() == "Windows" else "clear", shell=True)
 
-def base58(address_hex):
-    alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
-    b58_string = ""
-    # Get the number of leading zeros
-    leading_zeros = len(address_hex) - len(address_hex.lstrip("0"))
-    # Convert hex to decimal
-    address_int = int(address_hex, 16)
-    # Append digits to the start of string
-    while address_int > 0:
-        digit = address_int % 58
-        digit_char = alphabet[digit]
-        b58_string = digit_char + b58_string
-        address_int //= 58
-    # Add ‘1’ for each 2 leading zeros
-    ones = leading_zeros // 2
-    for one in range(ones):
-        b58_string = "1" + b58_string
-    return b58_string
+def wif_to_key(wif):
+    slicer = 4
+    if wif[0] in ['K', 'L']:
+        slicer = 5
+
+    return hexlify(b58decode(wif)[1:-slicer]).decode('utf-8')
 
 def skBinary():
     bin = input("Introduce the binary secret key: ")
     hex = format(int(bin, 2), 'x').upper()
     b58 = bitcoin.hex_to_b58check(hex, magicbyte=128)
-    wif = base58(b58)
+    pub_key = wif_to_key(b58)
     print(BRed + "PRIVATE INFORMATION (DO NOT SHARE WITH ANYONE):")
     print(BRed + "The binary secret key is:" + Red, bin)
     print(BRed + "The hexadecimal secret key is:" + Red, hex)
     print(BRed + "The B58Check WIF compressed secret key is:" + Red, b58)
-    print(BRed + "WIF (public key) is:" + Red, wif)
+    print(BGreen + "PUBLIC INFORMATION (SAFE TO SHARE)")
+    print(BGreen + "WIF (public key) is:" + Green, pub_key)
     quit()
 
 def skHex():
     hex = input("Introduce the hexadecimal secret key: ")
     bin = bin(int(hex, 16))[2:]
     b58 = bitcoin.hex_to_b58check(hex, magicbyte=128)
-    # wif = base58(b58)
+    # pub_wif = base58(b58)
     print(BRed + "PRIVATE INFORMATION (DO NOT SHARE WITH ANYONE):")
     print(BRed + "The binary secret key is:" + Red, bin)
     print(BRed + "The hexadecimal secret key is:" + Red, hex)
     print(BRed + "The B58Check WIF compressed secret key is:" + Red, b58)
-    # print(BRed + "WIF (public key) is:" + Red, wif)
+    # print(BRed + "WIF (public key) is:" + Red, pub_wif)
     quit()
 
 def skB58():
